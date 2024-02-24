571e99b6d85778910794e5a072ab1ec01af01c70
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-02-23 22:24:56 -0300

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,50 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/HeroSection.jsx";
+import Skills from "./components/Skills.jsx";
+import Education from "./components/Education.jsx";
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <ThemeProvider theme={darkTheme}>
+        <Router>
+          <Navbar />
+
+          <Body>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              <Education />
+            </Wrapper>
+          </Body>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
